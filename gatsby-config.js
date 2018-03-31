8789291f757cf4d5ccb6b1728b49ed2b8a560b46
Vitaliy Stanyshevskyy <vstanyshevskyy@stelladot.com>
2018-03-31 17:05:52 +0300

    Create articles filesystem resolver

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-netlify-cms'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-netlify-cms',
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/content/articles`,
+        name: 'articles',
+      },
+    },
+  ],
 };

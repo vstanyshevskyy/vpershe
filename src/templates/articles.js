4289f2c3ba4f3b17db45fcf7b64f37602d5e5884
Vitaliy Stanyshevskyy <vstanyshevskyy@stelladot.com>
2018-03-31 19:06:59 +0300

    Fixing build

--- src/templates/articles.js
+++ src/templates/articles.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import graphql from 'graphql'
 import Link from 'gatsby-link'
-import translit from 'cyrillic-to-translit-js'
+// import translit from 'cyrillic-to-translit-js'
 
 export default function Template ({ data }) {
   const post = data.markdownRemark.frontmatter;
-  const ukTranslit = translit({ preset: "uk" }).transform;
+  // const ukTranslit = translit({ preset: "uk" }).transform;
   return (
     <div>
       <h1>{post.title}</h1>
-      <div>Category: <Link to={`/categories/${ukTranslit(post.category)}`}>{post.category}</Link></div>
+      {/* <div>Category: <Link to={`/categories/${ukTranslit(post.category)}`}>{post.category}</Link></div> */}
       <div className="content" dangerouslySetInnerHTML={{__html: data.markdownRemark.html}}></div>
     </div>
   )
--- src/templates/categories-index.js
+++ src/templates/categories-index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import graphql from 'graphql'
 import Link from 'gatsby-link'
-import translit from 'cyrillic-to-translit-js'
+// import translit from 'cyrillic-to-translit-js'
 
 export default function Template ({ data }) {
   const post = data.markdownRemark.frontmatter;
-  const ukTranslit = translit({ preset: "uk" }).transform;
+  // const ukTranslit = translit({ preset: "uk" }).transform;
   return (
     <div>
       

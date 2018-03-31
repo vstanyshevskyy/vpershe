61621e3c3ec7752dbd430c4494eecb4aa0b51af7
Vladyslava <kurbanova.vladislava@gmail.com>
2018-03-31 19:57:31 +0300

    updated archive page

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -1,25 +1,80 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const ArchivePage = () => (
+const ArchivePage = ({data}) => 
+{
+  const articles = data.allMarkdownRemark.edges.map(edge => edge.node.frontmatter);
+  
+  const categories = groupByCategory(articles, "category").map(c => addCategory(c));
+  console.log(categories);
+  
+  return (
   <div>
     <h1>Archive</h1>
     <p>Welcome to Archive</p>    
-    <ul>{linksList}</ul>
+    {categories}
 
     <Link to="/">Go back to the homepage</Link>
   </div>
-)
+)};
 
-const articles = [{id: 1 , name: 'New article'}, {id: 2 , name: 'Second article'}, ];
+const addCategory = (category) => {
 
-const linksList = articles.map((article) => {
+  const posts = category.articles.map((a) => addPost(a));
+
   return (
-    <li style={{
-      color: 'white',
+    <div>
+      <h2>{category.name}</h2>
+      <ul>{posts}</ul>
+    </div>
+  )};
+
+
+const addPost = (article) => {
+  console.log(article);
+  return (
+    <li  style={{
+      color: 'red',
       textDecoration: 'none',
-    }}><Link to={"/article"+article.id}>Post : { article.name }</Link></li>
+    }}><Link to={"/article/"+ article.path}>Post : { article.title }</Link>
+  </li>
   )
-});
+};
+
+const groupByCategory = (items, key) =>{ 
+    let groups = [];
+    items.forEach(
+      (item) => {
+        var category = groups.find(r => r.name === item[key]);
+        if(category)
+        {
+          category.articles.push(item);  
+        } else {
+          groups.push({
+            name: item[key],
+            articles: [item]
+          })
+        }
+      });
+      return groups;
+};
 
 export default ArchivePage
+
+export const pageQuery = graphql`
+  query IndexQuery {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              path
+              title
+              category
+              tags        
+            }
+          }
+        }      
+    }
+  }
+`;
+

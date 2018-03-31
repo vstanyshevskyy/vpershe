30fc3d1184dfe0f470b6d2adcd9060bd21d35e49
Vitaliy Stanyshevskyy <vstanyshevskyy@stelladot.com>
2018-03-31 19:17:09 +0300

    Fix 2

--- src/templates/categories-index.js
+++ src/templates/categories-index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 // import translit from 'cyrillic-to-translit-js'
 
 export default function Template ({ data }) {
-  const post = data.markdownRemark.frontmatter;
+  // const post = data.markdownRemark.frontmatter;
   // const ukTranslit = translit({ preset: "uk" }).transform;
   return (
     <div>

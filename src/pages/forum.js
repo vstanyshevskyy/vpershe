236f9f542694509741242c009ea59c2bb246295b
Vitaliy Stanyshevskyy <vstanyshevskyy@stelladot.com>
2018-04-01 17:13:40 +0300

    Disable forum to fix build

--- src/pages/forum.js
+++ src/pages/forum.js
@@ -1,44 +1,45 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import ReactDisqusThread from 'react-disqus-thread';
+// import ReactDisqusThread from 'react-disqus-thread';
 
 export default class Forum extends React.Component {
 	constructor() {
 		super();
-		this.state = {
-			forumConfig: {
-				shortname: "forumConfig",
-				title: "Вперше - форум",
-				url: "http://www.example.com/forum"
-			},
-			commentsHistory: []
-		}
+	// 	this.state = {
+	// 		forumConfig: {
+	// 			shortname: "forumConfig",
+	// 			title: "Вперше - форум",
+	// 			url: "http://www.example.com/forum"
+	// 		},
+	// 		commentsHistory: []
+	// 	}
 	}
-	componentDidMount() {
-		this.setState({
-			commentsHistory: JSON.parse(localStorage.getItem('commentsHistory'))
-		});
-	}
-	updateCommentsHistory() {
-		let commentsHistory = null;
-		if (!this.state.commentsHistory) {
-			localStorage.setItem('commentsHistory', JSON.stringify(this.state.commentsHistory));
-		} else{
-			commentsHistory = JSON.parse(localStorage.getItem('commentsHistory'));
-		}
-	}
-	handleNewComment(comment) {
-		console.log(comment.text);
-	}
+	// componentDidMount() {
+	// 	this.setState({
+	// 		commentsHistory: JSON.parse(localStorage.getItem('commentsHistory'))
+	// 	});
+	// }
+	// updateCommentsHistory() {
+	// 	let commentsHistory = null;
+	// 	if (!this.state.commentsHistory) {
+	// 		localStorage.setItem('commentsHistory', JSON.stringify(this.state.commentsHistory));
+	// 	} else{
+	// 		commentsHistory = JSON.parse(localStorage.getItem('commentsHistory'));
+	// 	}
+	// }
+	// handleNewComment(comment) {
+	// 	console.log(comment.text);
+	// }
 	render() {
 		return (
-			<ReactDisqusThread
-				shortname={this.state.forumConfig.shortname}
-				identifier={this.state.forumConfig.shortname}
-				title={this.state.forumConfig.title}
-				url={this.state.forumConfig.url}
-				category_id="123456"
-				onNewComment={this.handleNewComment}/>
+			<div></div>
+			// <ReactDisqusThread
+			// 	shortname={this.state.forumConfig.shortname}
+			// 	identifier={this.state.forumConfig.shortname}
+			// 	title={this.state.forumConfig.title}
+			// 	url={this.state.forumConfig.url}
+			// 	category_id="123456"
+			// 	onNewComment={this.handleNewComment}/>
 		);
 	}
 }

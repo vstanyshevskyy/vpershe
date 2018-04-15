08c97de3d5654bfb21b0c3237b81383118a6ba78
Vitaliy Stanyshevskyy <vstanyshevskyy@stelladot.com>
2018-04-15 13:59:31 +0300

    Create footer component and embed it

---
+++ src/components/footer/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Link from 'gatsby-link';
+import { Container, Row, Col } from 'reactstrap';
+import './index.less';
+
+const BOOTSTRAP_COLUMNS_COUNT = 12;
+
+export default props => (
+  <footer className="container-fluid footer">
+    <Container>
+      <Row>
+        {
+          props.blocks.map((block, index) => (
+            <Col key={index} xs={12} lg={BOOTSTRAP_COLUMNS_COUNT / props.blocks.length}>
+              <h5 className="block-title">{ block.title }</h5>
+              <div dangerouslySetInnerHTML={{ __html: block.content }} />
+            </Col>
+          ))
+        }
+      </Row>
+    </Container>
+    <Row className="separator" />
+    <Container className="footer-bottom">
+      <Row>
+        <Col sm="8" xs="12" >
+          {props.copyrightText}
+        </Col>
+        <Col xs="12" sm="4" className="text-sm-right">
+          {props.bottomLinks.map(link => (
+            <Link key={link.url} to={link.url}>{link.text}</Link>
+          ))}
+        </Col>
+      </Row>
+    </Container>
+  </footer>
+);
+

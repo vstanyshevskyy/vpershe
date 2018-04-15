e908aba31748d4841a0c89b49159a361c3f1546f
Vitaliy Stanyshevskyy <vstanyshevskyy@stelladot.com>
2018-04-15 13:31:43 +0300

    Add settings folder gatsby-source settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,34 @@
 module.exports = {
-	siteMetadata: {
-		title: 'Vpershe Site',
-	},
-	plugins: [
-		'gatsby-plugin-react-helmet',
-		'gatsby-plugin-netlify-cms',
-		'gatsby-remark-responsive-iframe',
-		'gatsby-transformer-remark',
-		'gatsby-plugin-less',
-		{
-			resolve: 'gatsby-source-filesystem',
-			options: {
-				path: `${__dirname}/content/articles`,
-				name: 'articles',
-			},
-		},
-		{
-			resolve: 'gatsby-plugin-mailchimp',
-			options: {
-				hostname: 'string',
-				listId: 'string',
-				settings: 'object'
-			}
-		}
-	],
+  siteMetadata: {
+    title: 'Vpershe Site'
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-netlify-cms',
+    'gatsby-remark-responsive-iframe',
+    'gatsby-transformer-remark',
+    'gatsby-plugin-less',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/content/articles`,
+        name: 'articles'
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'settings',
+        path: `${__dirname}/content/settings/`
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-mailchimp',
+      options: {
+        hostname: 'string',
+        listId: 'string',
+        settings: 'object'
+      }
+    }
+  ]
 };

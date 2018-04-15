f58de9d173b3db75910076745fa488dd624e757a
Vitaliy Stanyshevskyy <vstanyshevskyy@stelladot.com>
2018-04-15 13:32:57 +0300

    Do not generate page for footer

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,20 @@
-const path = require('path')
+const path = require('path');
+
+const SKIP_TYPES = [
+  'footer_settings'
+];
 
 exports.createPages = ({ boundActionCreators, graphql }) => {
-	const { createPage } = boundActionCreators
+  const { createPage } = boundActionCreators;
 
-	return graphql(`
+  return graphql(`
     {
       allMarkdownRemark {
         edges {
           node {
             frontmatter {
-							path
-							image
+              path
+              image
               category
               tags
               contentType
@@ -20,49 +24,51 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
       }
     }
   `).then(result => {
-		if (result.errors) {
-			return Promise.reject(result.errors)
-		}
-		console.log(result);
-		console.log(JSON.stringify(result.data.allMarkdownRemark));
-		const categories = [];
-		const tags = [];
-		result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-			if (categories.indexOf(node.frontmatter.category) === -1){
-				categories.push(node.frontmatter.category);
-			}
-			node.frontmatter.tags.forEach(tag => {
-				if (tags.indexOf(tag)===-1){
-					tags.push(tag);
-				}
-			})
-			createPage({
-				path: node.frontmatter.contentType + '/' + node.frontmatter.path,
-				component: path.resolve(`src/templates/${node.frontmatter.contentType}.js`),
-				context: {
-					slug: node.frontmatter.path,
-				} // additional data can be passed via context
-			})
-		})
-		console.log(categories);
-		console.log(tags);
-		categories.forEach(category => {
-			createPage({
-				path: `categories/${category}`,
-				component: path.resolve(`src/templates/categories-index.js`),
-				context: {
-					category: category
-				}
-			})
-		})
-		tags.forEach(tag => {
-			createPage({
-				path: `tags/${tag}`,
-				component: path.resolve(`src/templates/tags-index.js`),
-				context: {
-					tag: tag
-				}
-			})
-		})
-	})
+    if (result.errors) {
+      return Promise.reject(result.errors);
+    }
+    console.log(JSON.stringify(result.data.allMarkdownRemark));
+    const categories = [];
+    const tags = [];
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      if (SKIP_TYPES.indexOf(node.frontmatter.contentType) > -1) {
+        return;
+      }
+      if (categories.indexOf(node.frontmatter.category) === -1) {
+        categories.push(node.frontmatter.category);
+      }
+      (node.frontmatter.tags || []).forEach(tag => {
+        if (tags.indexOf(tag) === -1) {
+          tags.push(tag);
+        }
+      });
+      createPage({
+        path: `${node.frontmatter.contentType}/${node.frontmatter.path}`,
+        component: path.resolve(`src/templates/${node.frontmatter.contentType}.js`),
+        context: {
+          slug: node.frontmatter.path
+        } // additional data can be passed via context
+      });
+    });
+    console.log(categories);
+    console.log(tags);
+    categories.forEach(category => {
+      createPage({
+        path: `categories/${category}`,
+        component: path.resolve('src/templates/categories-index.js'),
+        context: {
+          category
+        }
+      });
+    });
+    tags.forEach(tag => {
+      createPage({
+        path: `tags/${tag}`,
+        component: path.resolve('src/templates/tags-index.js'),
+        context: {
+          tag
+        }
+      });
+    });
+  });
 };

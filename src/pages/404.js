9f06bc38addf21b6978057a1184f46a3bbe7bf6b
Vitaliy Stanyshevskyy <stvitaliyvskiy@gmail.com>
2018-08-31 19:52:52 +0300

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <div className="index-page__content-wrapper">
+    <h1>Уууупс!</h1>
+    <img src="https://media.giphy.com/media/oqVsm9kaUm0ow/giphy.gif" alt="oops" />
+    <p>Такої сторінки в нас нема</p>
   </div>
 );
 

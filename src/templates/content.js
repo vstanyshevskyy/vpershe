0e741320014db4ced60294ce059ab5a66254e384
Vitaliy Stanyshevskyy <stvitaliyvskiy@gmail.com>
2018-09-28 14:54:30 +0300

    Add related links to content pages

--- src/templates/content.js
+++ src/templates/content.js
@@ -3,6 +3,8 @@ import { withPrefix } from 'gatsby-link';
 import moment from 'moment';
 import './content.less';
 import TagsList from '../components/tags';
+import Carousel from '../components/carousel';
+import RelatedLinks from '../components/realted-links';
 import SEO from '../components/SEO';
 
 export default props => {
@@ -11,22 +13,29 @@ export default props => {
     html: props.pathContext.data.html
   }, props.pathContext.data.frontmatter);
   const settings = props.pathContext.settings;
-  console.log(pageData);
+  const carouselItems = (pageData.related_bottom || []).map(c => c.node.frontmatter);
   return (
     <div className={`index-page__content-wrapper index-page__content-wrapper--${pageData.contentType}`}>
       <SEO {...{ data: settings }} />
-      <article>
+      <article className="content__article">
         { pageData.image
           ? <img className="content__img" src={withPrefix(pageData.image)} alt="" />
           : null }
-        <h1 className="content__title">{pageData.title}</h1>
-        <div className="content__subtitle">{pageData.subtitle}</div>
-        {
-          pageData.publishTime
-            ? <div className="content__date">{moment(pageData.publishTime).format('LL')}</div>
-            : null
-        }
-        <div className="content__content" dangerouslySetInnerHTML={{ __html: pageData.html }} />
+        <div className="content__article-head">
+          <h1 className="content__title">{pageData.title}</h1>
+          <div className="content__subtitle">{pageData.subtitle}</div>
+          {
+            pageData.publishTime
+              ? <div className="content__date">{moment(pageData.publishTime).format('LL')}</div>
+              : null
+          }
+        </div>
+        <div className="content__article-wrapper">
+          <div className="content__content" dangerouslySetInnerHTML={{ __html: pageData.html }} />
+          <aside className="content__sidebar">
+            <RelatedLinks links={pageData.related_sidebar} />
+          </aside>
+        </div>
       </article>
       <aside>
         {
@@ -34,7 +43,12 @@ export default props => {
             ? <TagsList pageName={pageData.contentType} tags={pageData.tags} />
             : null
         }
+        <div className="content__related-items-bottom">
+          <h5 className="content__related-items-bottom-title">Схожі матеріали</h5>
+          <Carousel className="slider--content" items={carouselItems} />
+        </div>
       </aside>
+      
     </div>
   );
 };

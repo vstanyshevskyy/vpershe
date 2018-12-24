8aca2c5e42dbc147e873f5d6b98195d929e79a1d
Vitaliy Stanyshevskyy <stvitaliyvskiy@gmail.com>
2018-12-24 20:24:46 +0200

    Add splashscreen

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -53,6 +53,16 @@ class Layout extends React.Component {
             <link href="/assets/icon-114x114.png" sizes="114x114" rel="apple-touch-icon" />
             <link href="/assets/icon-144x144.png" sizes="144x144" rel="apple-touch-icon" />
             <meta name="apple-mobile-web-app-capable" content="yes" />
+            <link href="/assets/splashscreen/iphone5_splash.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+            <link href="/assets/splashscreen/iphone6_splash.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+            <link href="/assets/splashscreen/iphoneplus_splash.png" media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+            <link href="/assets/splashscreen/iphonex_splash.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+            <link href="/assets/splashscreen/iphonexr_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+            <link href="/assets/splashscreen/iphonexsmax_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+            <link href="/assets/splashscreen/ipad_splash.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+            <link href="/assets/splashscreen/ipadpro1_splash.png" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+            <link href="/assets/splashscreen/ipadpro3_splash.png" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+            <link href="/assets/splashscreen/ipadpro2_splash.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
           </Helmet>
           <Navbar location={location.pathname} className={location.pathname.split('/')[1]} {...navbarSettings} />
           {

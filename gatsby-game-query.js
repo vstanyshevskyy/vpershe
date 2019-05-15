5d234f9ccf97eac69b764e363b3595cf70f0c34d
vstanyshevskyy <stvitaliyvskiy@gmail.com>
2019-05-15 14:34:55 +0300

    Get game, calculate the percentage, pass to the page

--- gatsby-game-query.js
+++ gatsby-game-query.js
@@ -6,40 +6,92 @@ module.exports = (createPage, graphql) => {
 
   // TODO: add bannerUrl query property
   return graphql(`
-    {
-      games: allMarkdownRemark (
-        filter: { frontmatter: { contentType: { eq: "games" } } }
-      ) {
-        edges {
-          node {
-            frontmatter {
-              path
+  {
+    games: allMarkdownRemark(filter: {frontmatter: {contentType: {eq: "games"}}}) {
+      edges {
+        node {
+          frontmatter {
+            path
+            title
+            contentType
+            options {
+              buttonText
               title
-              contentType
+              image
+              link
+              options {
+                buttonText
+                title
+                link
+                image
+                options {
+                  buttonText
+                  title
+                  image
+                  link
+                  options {
+                    buttonText
+                    title
+                    image
+                    link
+                    options {
+                      buttonText
+                      title
+                      image
+                      link
+                      options {
+                        buttonText
+                      }
+                    }
+                  }
+                }
+              }
             }
           }
         }
       }
-    }`).then(result => {
+    }
+  }  
+  `).then(result => {
     if (result.errors) {
       throw result.errors;
     }
-    const contentItems = result.data.games;
-    createPage({
-      path: 'games',
-      component: gamesPage,
-      context: {
-        contentItems
-      }
-    });
-    result.data.games.edges.forEach(edge => {
+    result.data.games.edges.map(gameEdge => {
+      const dfs = (node, stepDepth = 0) => {
+        const depths = node.options ? node.options.map(n => {
+          return dfs(n, stepDepth + 1);
+        }) : [stepDepth];
+        const stepMaxDepth = Math.max(...depths);
+        node.percentCompleted = Math.floor(stepDepth / stepMaxDepth * 100);
+        return stepMaxDepth;
+      };
+      dfs(gameEdge.node.frontmatter);
+
+      console.log(JSON.stringify(gameEdge.node.frontmatter, null, 2));
       createPage({
-        path: `games/${edge.node.frontmatter.path}`,
+        path: `games/${gameEdge.node.frontmatter.path}`,
         component: gameItemPage,
         context: {
-          contentType: edge
+          game: gameEdge.node.frontmatter
         }
       });
     });
+    // const contentItems = result.data.games;
+    // createPage({
+    //   path: 'games',
+    //   component: gamesPage,
+    //   context: {
+    //     contentItems
+    //   }
+    // });
+    // result.data.games.edges.forEach(edge => {
+    //   createPage({
+    //     path: `games/${edge.node.frontmatter.path}`,
+    //     component: gameItemPage,
+    //     context: {
+    //       contentType: edge
+    //     }
+    //   });
+    // });
   });
 };

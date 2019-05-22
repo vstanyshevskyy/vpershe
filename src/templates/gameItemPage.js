f099421a7601d0fb02874d193b1bb31ef3198374
vstanyshevskyy <stvitaliyvskiy@gmail.com>
2019-05-22 20:36:21 +0300

    Fix eslint issues

--- src/templates/gameItemPage.js
+++ src/templates/gameItemPage.js
@@ -1,45 +1,41 @@
-import React from 'react'
+import React from 'react';
 
-import Layout from '../layouts'
-import './game.less'
+import Layout from '../layouts';
+import './game.less';
 
 const transformPageContextItemToGameItem = options => {
-  const game = options.pageContext.game
+  const { game } = options.pageContext;
 
   return {
     path: `games/${game.path}`,
-    ...game,
-  }
-}
+    ...game
+  };
+};
 
 export const GameChoice = props => {
-  const { buttonText, onChoiceAnswer } = props
+  const { buttonText, onChoiceAnswer } = props;
 
-  return <button onClick={onChoiceAnswer}>{buttonText}</button>
-}
+  return <button onClick={onChoiceAnswer} type="button">{buttonText}</button>;
+};
 
-export const GameProgressBar = props => {
-  return (
-    <div className="progressBar">
-      <span className="track" style={{ width: `${props.progress}%` }} />
-    </div>
-  )
-}
+export const GameProgressBar = props => (
+  <div className="progressBar">
+    <span className="track" style={{ width: `${props.progress}%` }} />
+  </div>
+);
 
-export const GameUsefullLink = props => {
-  return (
-    <p className="usefullLink">
-      <img
-        src="/assets/uploads/illustration-final.png"
-        title="game image"
-        height={50}
-      />
-      <a href={props.link} target="blank">
+export const GameUsefullLink = props => (
+  <p className="usefullLink">
+    <img
+      src="/assets/uploads/illustration-final.png"
+      alt="link"
+      height={50}
+    />
+    <a href={props.link} target="blank">
         Рекомендованна сттатя
-      </a>
-    </p>
-  )
-}
+    </a>
+  </p>
+);
 
 export default class Game extends React.Component {
   state = {
@@ -48,38 +44,43 @@ export default class Game extends React.Component {
     percentCompleted: 0,
     options: [],
     image: '',
-    link: '',
+    link: ''
   }
+
   initialState = {}
 
   componentDidMount() {
-    const game = transformPageContextItemToGameItem(this.props)
-    this.initialState = game
+    const game = transformPageContextItemToGameItem(this.props);
+    this.initialState = game;
     this.setState({
       title: game.title,
       options: game.options,
       image: game.image
-    })
+    });
   }
 
   onChoiceAnswer(data) {
-    const { title, percentCompleted, options, link, image } = data
-    this.setState({ title, percentCompleted, options, link, image })
+    const {
+      title, percentCompleted, options, link, image
+    } = data;
+    this.setState({
+      title, percentCompleted, options, link, image
+    });
   }
 
   startGame() {
-    this.setState({ isStarted: true })
+    this.setState({ isStarted: true });
   }
 
   restartGame() {
-    const { options, title } = this.initialState
+    const { options, title } = this.initialState;
     this.setState({
       title,
       options,
       percentCompleted: 0,
       link: '',
-      image: '',
-    })
+      image: ''
+    });
   }
 
   render() {
@@ -89,11 +90,11 @@ export default class Game extends React.Component {
       options,
       link,
       image,
-      isStarted,
-    } = this.state
+      isStarted
+    } = this.state;
 
     if (!this.props) {
-      return null
+      return null;
     }
 
     return (
@@ -109,7 +110,7 @@ export default class Game extends React.Component {
           <section>
             <img
               src={image}
-              title="game image"
+              alt="game"
               height={250}
             />
             {isStarted ? (
@@ -127,14 +128,14 @@ export default class Game extends React.Component {
                 </ul>
               </>
             ) : (
-              <button onClick={() => this.startGame()}>Грати</button>
+              <button onClick={() => this.startGame()} type="button">Грати</button>
             )}
           </section>
           <footer>
-            {percentCompleted == 100 && (
+            {percentCompleted === 100 && (
               <div>
                 {link && <GameUsefullLink link={link} />}
-                <button onClick={() => this.restartGame()}>
+                <button type="button" onClick={() => this.restartGame()}>
                   Спробувати ще раз
                 </button>
               </div>
@@ -142,6 +143,6 @@ export default class Game extends React.Component {
           </footer>
         </div>
       </Layout>
-    )
+    );
   }
 }

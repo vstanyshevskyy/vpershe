a4c7aa17dd3b92cf9eb86b54ff6474cb49717c3c
vstanyshevskyy <stvitaliyvskiy@gmail.com>
2020-01-17 19:15:04 +0200

    Refactoring: move article tiles to smart component. rename recent stories folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import { graphql, Link, withPrefix } from 'gatsby';
+import { withPrefix } from 'gatsby';
 import './index.less';
 import Layout from '../layouts';
 import SEO from '../components/SEO';
 import Subscribe from '../components/subscribe';
 import Carousel from '../components/carousel';
 import RecentAdviceList from '../components/recent-advice-list';
-import ArticlesTiles from '../components/articles-tiles';
-import RecentStories from '../components/recent-stories';
+import ArticlesTiles from '../components/recent-articles-tiles';
+import RecentStories from '../components/recent-stories-list';
 import AboutProject from '../components/about-project';
 
-export default function Template ({ data }) {
-  const articles = ((data.articles || {}).edges || []).map(c => c.node.frontmatter);
+export default function Template () {
   return (
     <Layout>
       <div id="content">
         <SEO />
         <Carousel />
-        <ArticlesTiles items={articles} />
+        <ArticlesTiles />
         <img className="homepage__graffiti homepage__graffiti--eye" loading="lazy" alt="" width="76" src={withPrefix('assets/graffiti/eye.svg')} aria-hidden="true" />
         <RecentStories />
         <div className="homepage__graffiti-wrapper">
@@ -31,33 +30,3 @@ export default function Template ({ data }) {
     </Layout>
   );
 }
-
-export const pageQuery = graphql`
-query HomePage {
-  articles: allMarkdownRemark(
-    filter: { frontmatter:  { contentType: { eq: "articles"} }}
-    sort: { fields: [frontmatter___publishTime], order: DESC }
-    limit: 2
-  ){
-    edges{
-      node{
-        frontmatter {
-          title
-          contentType
-          path
-          subtitle
-          image {
-            relativePath
-            childImageSharp {
-              fluid(maxWidth: 533, maxHeight: 350) {
-                ...GatsbyImageSharpFluid_tracedSVG
-              }
-            }
-          }
-          image_alt
-        }
-      }
-    }
-  }
-}
-`;

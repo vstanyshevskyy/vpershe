fbd3e240b6560dbed6c8f94ba7c3279cec6fb68b
vstanyshevskyy <stvitaliyvskiy@gmail.com>
2020-01-31 17:43:13 +0200

    use uk locale for admin

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,10 +1,15 @@
 import CMS from 'netlify-cms-app';
+import { uk } from 'netlify-cms-locales';
 import config from './config';
 
 /**
  * Optionally pass in a config object. This object will be merged into `config.yml` if it exists
  */
+CMS.registerLocale('uk', uk);
 
 CMS.init({
-  config
+  config: {
+    ...config,
+    locale: 'uk'
+  }
 });
